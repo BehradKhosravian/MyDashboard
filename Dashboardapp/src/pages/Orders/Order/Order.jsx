a4901a90bf06bc7c183fe76de571568e7df30176
Behrad <behradkhosraviandev@gmail.com>
2024-02-03 00:49:20 +0330

    5th: make a simple orderslist

--- Dashboardapp/src/pages/Orders/Order/Order.jsx
+++ Dashboardapp/src/pages/Orders/Order/Order.jsx
@@ -1,9 +1,23 @@
 import { orderList } from "../../../Data";
+import threedots from "../../../assets/icon/threedots.svg";
 function Order() {
   return (
-    <div className="flex justify-around">
+    <div>
       {orderList.map((order) => (
-        <p key={order.id}>{order.id}</p>
+        <div className="p-4 border-b-2  border-[#E6EDFF]" key={order.id}>
+          <div className="flex justify-around ">
+            <p>{order.no}</p>
+            <p>{order.id}</p>
+            <p>{order.date}</p>
+            <p>{order.name}</p>
+            <p>{order.location}</p>
+            <p>{order.amount}</p>
+            <p>{order.status}</p>
+            <button>
+              <img src={threedots} alt="Action" />
+            </button>
+          </div>
+        </div>
       ))}
     </div>
   );
--- Dashboardapp/src/pages/Orders/Orders.jsx
+++ Dashboardapp/src/pages/Orders/Orders.jsx
@@ -3,9 +3,9 @@ function Orders() {
   return (
     <div className="orders-container px-6 w-full   ">
       <div className=" border p-6  border-[#E6EDFF] rounded-xl ">
-        <div className="orders-header border-b-2 border-[#E6EDFF]">
-          <h1 className="font-semibold text-lg mb-10">Order List</h1>
-          <div className="orders-header-list flex justify-around mb-3">
+        <div className="orders-header ">
+          <h1 className="font-semibold text-2xl mb-10">Order List</h1>
+          <div className="orders-header-list border-b-2 border-[#E6EDFF] flex justify-around pb-4">
             <h1>No</h1>
             <h1>ID</h1>
             <h1>Date</h1>
