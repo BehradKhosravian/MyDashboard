3026e713ab0aadf241056d7b996a2f664a6d2bce
Behrad <behradkhosraviandev@gmail.com>
2024-02-08 21:16:59 +0330

    8th: fix the pagination of table now want to make it a component

--- Dashboardapp/src/pages/Orders/Order/Order.jsx
+++ Dashboardapp/src/pages/Orders/Order/Order.jsx
@@ -1,24 +1,75 @@
 import { orderList } from "../../../Data";
 import threedots from "../../../assets/icon/threedots.svg";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
 // border-[#E6EDFF]
 function Order() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordsPerPage = 10;
+  const lastIndex = currentPage * recordsPerPage;
+  const firstIndex = lastIndex - recordsPerPage;
+  const records = orderList.slice(firstIndex, lastIndex);
+  const npage = Math.ceil(orderList.length / recordsPerPage);
+  const numbers = [...Array(npage + 1).keys()].slice(1);
+
+  function prePage() {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  function changePage(id) {
+    setCurrentPage(id);
+  }
+
+  function nextPage() {
+    if (currentPage  !== 10) {
+      setCurrentPage(currentPage + 1);
+      console.log(currentPage);
+    }
+  }
+
   return (
-    <tbody>
-      {orderList.map((order) => (
-        <tr className="  border-b-2 border-[#E6EDFF] " key={order.id}>
-          <td className="p-2 align-middle text-center">{order.no}</td>
-          <td className="p-2 align-middle text-center">{order.id}</td>
-          <td className="p-2 align-middle text-center">{order.date}</td>
-          <td className="p-2 align-middle text-center">{order.name}</td>
-          <td className="p-2 align-middle text-center">{order.location}</td>
-          <td className="p-2 align-middle text-center">{order.amount}</td>
-          <td className="p-2 align-middle text-center">{order.status}</td>
-          <button className="ps-10 py-2 align-middle">
-            <img src={threedots} alt="Action" />
-          </button>
-        </tr>
-      ))}
-    </tbody>
+    <>
+      <tbody>
+        {records.map((order) => (
+          <tr className="  border-b-2 border-[#E6EDFF] " key={order.id}>
+            <td className="p-2 align-middle text-center">{order.no}</td>
+            <td className="p-2 align-middle text-center">{order.id}</td>
+            <td className="p-2 align-middle text-center">{order.date}</td>
+            <td className="p-2 align-middle text-center">{order.name}</td>
+            <td className="p-2 align-middle text-center">{order.location}</td>
+            <td className="p-2 align-middle text-center">{order.amount}</td>
+            <td className="p-2 align-middle text-center">{order.status}</td>
+            <button className="ps-10 py-2 align-middle">
+              <img src={threedots} alt="Action" />
+            </button>
+          </tr>
+        ))}
+      </tbody>
+      <div className="mt-3 mb-12 ">
+        <ul className="">
+          <li>
+            <NavLink to="" onClick={() => prePage()}>
+              Prev
+            </NavLink>
+          </li>
+          {numbers.map((number, i) => (
+            <li key={i}>
+              <NavLink to="" onClick={() => changePage(number)}>
+                {number}
+              </NavLink>
+            </li>
+          ))}
+          <li>
+            <NavLink to="" onClick={() => nextPage()}>
+              Next
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 
--- Dashboardapp/src/pages/Orders/Orders.jsx
+++ Dashboardapp/src/pages/Orders/Orders.jsx
@@ -3,26 +3,35 @@
 // import Order from "./Order/Order";
 // import Pagination from "@mui/material/Pagination";
 
+import { useState } from "react";
 import Order from "./Order/Order";
+import { orderList } from "../../Data";
+// import Pagination from "../../components/Pagination";
+import { NavLink } from "react-router-dom";
 
 // border-[#E6EDFF]
 function Orders() {
+
+
   return (
-    <table className="w-full text-sm lg:text-lg mx-8">
-      <thead>
-        <tr className="border-b-2 border-[#E6EDFF]">
-          <th className="px-3 py-2">No</th>
-          <th className="px-3 py-2">ID</th>
-          <th className="px-3 py-2">Date</th>
-          <th className="px-3 py-2">Customer Name</th>
-          <th className="px-3 py-2">Location</th>
-          <th className="px-3 py-2">Amount</th>
-          <th className="px-3 py-2">Status Order</th>
-          <th className="px-3 py-2">Action</th>
-        </tr>
-      </thead>
-      <Order />
-    </table>
+    <>
+      <table className="w-full text-sm lg:text-lg mx-8">
+        <thead>
+          <tr className="border-b-2 border-[#E6EDFF]">
+            <th className="px-3 py-2">No</th>
+            <th className="px-3 py-2">ID</th>
+            <th className="px-3 py-2">Date</th>
+            <th className="px-3 py-2">Customer Name</th>
+            <th className="px-3 py-2">Location</th>
+            <th className="px-3 py-2">Amount</th>
+            <th className="px-3 py-2">Status Order</th>
+            <th className="px-3 py-2">Action</th>
+          </tr>
+        </thead>
+        <Order />
+      </table>
+
+    </>
   );
 }
 
